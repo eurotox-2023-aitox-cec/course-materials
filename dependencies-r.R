be52e03b91fe1ad35932d1c5de5d80f91d6e24f2
Thomas Luechtefeld <tom@insilica.co>
2023-09-05 13:45:47 -0400

    add deps for track-a/prob-hazard-models
    added dependencies for track-a / prob-hazard-models

--- dependencies-r.R
+++ dependencies-r.R
@@ -23,3 +23,30 @@ pkg_install(c(
   "GGally", # toxicogenomics
   "enrichR"# toxicogenomics
 ))
+
+# Track A / Prob-Hazard-Models ==========================================================
+
+# Install reticulate first to manage Python dependencies
+install.packages("reticulate") 
+
+# needs a python environment, two ways to do this:
+# Virtualenv:
+#    > `python -m venv reticulate` # in bash shell
+#    > `source venv/bin/activate` 
+#    > `pip install rdkit pillow biobricks`
+# reticulate: # this method won't work on all systems
+#    > reticulate::virtualenv_create('reticulate')
+#    > reticulate::use_virtualenv('reticulate',required=TRUE)
+#    > reticulate::py_install(c('rdkit','biobricks','pillow'), method='pip')
+
+# Install R packages needed for prob-hazard-models
+pkg_install(c(
+  "pacman",  # For package management
+  "httr",    # For HTTP requests
+  "jsonlite", # For JSON parsing
+  "arrow",   # For data interchange
+  "glue",    # For string manipulation
+  "torch",   # For deep learning
+  "zeallot", # For unpacking
+  "github::biobricks-ai/biobricks-r"  # For biobricks R package from GitHub
+))
